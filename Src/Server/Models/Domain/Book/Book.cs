7f70c4ec0c39b10dc659f87cf28f92a37e8ea2e0
Alger <AlgerDu@outlook.com>
2017-04-05 00:12:11 +0800

    添加注释

--- Src/Server/Models/Domain/Book/Book.cs
+++ Src/Server/Models/Domain/Book/Book.cs
@@ -3,6 +3,9 @@ using MyZone.Server.Infrastructure.Interface;
 
 namespace MyZone.Server.Models.DataBase
 {
+    /// <summary>
+    /// 书籍领域模型
+    /// </summary>
     public partial class Book : IAggregateRoot<Guid>
     {
         public Guid Key
