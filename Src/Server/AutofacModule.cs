df172f321ccf4c9b94bc866506b2883cf8700caf
Alger <AlgerDu@outlook.com>
2017-04-06 00:06:22 +0800

    使用 autofac

---
+++ Src/Server/AutofacModule.cs
@@ -0,0 +1,11 @@
+using Autofac;
+
+namespace MyZone.Server
+{
+    public class AutofacModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+        }
+    }
+}
