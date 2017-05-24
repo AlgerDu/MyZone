a0d8b97fdb93077b8dd67ef21ea814d3e81f4ff3
Alger <AlgerDu@outlook.com>
2017-05-24 23:50:53 +0800

    添加接口

--- Src/Server/Controllers/BookManagementController.cs
+++ Src/Server/Controllers/BookManagementController.cs
@@ -1,5 +1,8 @@
+using System;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using MyZone.Server.Infrastructure.Interface;
+using MyZone.Server.Models.DTO.NovelCrawl;
 
 namespace MyZone.Server.Controllers
 {
@@ -15,6 +18,18 @@ namespace MyZone.Server.Controllers
         )
         {
             _logger = logger;
+        }
+
+        /// <summary>
+        /// 向系统中添加一本小说类型的书籍
+        /// </summary>
+        /// <param name="novle"></param>
+        /// <returns></returns>
+        public IResult<Guid> AddNovle(
+            [FromBody]NovelAddModel novle
+        )
+        {
+            return null;
         }
     }
 }
---
+++ Src/Server/Models/DTO/BookManagement/NovelAddModel.cs
@@ -0,0 +1,12 @@
+namespace MyZone.Server.Models.DTO.BookManagement
+{
+    /// <summary>
+    /// 添加小说书籍
+    /// </summary>
+    public class NovelAddModel
+    {
+        public string Name { get; set; }
+
+        public string Author { get; set; }
+    }
+}
