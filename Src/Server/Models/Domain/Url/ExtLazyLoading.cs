72333ff5fcaf84e221975f730b8489d85efaaef8
Alger <AlgerDu@outlook.com>
2017-05-31 23:08:02 +0800

    Url领域模型

---
+++ Src/Server/Models/Domain/Url/ExtLazyLoading.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Linq;
+using MyZone.Server.Infrastructure.Interface;
+using MyZone.Server.Models.DataBase;
+
+namespace MyZone.Server.Models.Domain.Urls
+{
+    public static class ExtLazyLoading
+    {
+        /// <summary>
+        /// 加载 url 对应的解析代码
+        /// </summary>
+        /// <param name="lazy"></param>
+        /// <param name="url"></param>
+        public static void LoadUrlPageParse(this IFunnyLazyLoading lazy, Url url)
+        {
+            var context = lazy.Context as MyZoneContext;
+            url.Parse = context.PageParse
+                    .FirstOrDefault(pp => pp.Url == url.UrlPath);
+        }
+
+        public static void LoadUrlHost(this IFunnyLazyLoading lazy, Url url)
+        {
+            var context = lazy.Context as MyZoneContext;
+
+            var host = context.Host
+                    .FirstOrDefault(h => h.Name == url.HostName);
+
+            if (host == null)
+            {
+                host = new Host()
+                {
+                    Name = url.HostName,
+                    Uid = Guid.NewGuid()
+                };
+
+                context.Host.Add(host);
+            }
+
+            host.PageParses = context.PageParse
+                    .Where(pp => pp.Url == host.Name);
+        }
+    }
+}
--- Src/Server/Models/Domain/Url/Url.cs
+++ Src/Server/Models/Domain/Url/Url.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using MyZone.Server.Infrastructure.Interface;
 
 namespace MyZone.Server.Models.DataBase
@@ -14,5 +15,24 @@ namespace MyZone.Server.Models.DataBase
                 return UrlPath;
             }
         }
+
+        /// <summary>
+        /// 获取主机名
+        /// </summary>
+        public string HostName
+        {
+            get
+            {
+                var array = Regex.Split(UrlPath, @"(^http[s]?://[^/:]+(:\d*)?)");
+                return array[0];
+            }
+        }
+
+        /// <summary>
+        /// 主机
+        /// </summary>
+        public Host Host { get; set; }
+
+        public PageParse Parse { get; set; }
     }
 }
