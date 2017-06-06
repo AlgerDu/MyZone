45f6e8bc1c769f2280d861ad3e72cb5f79a1a63c
Alger <AlgerDu@outlook.com>
2017-06-06 22:47:46 +0800

    接口定义

--- Src/Server/Infrastructure/Interface/IServicePrivoder.cs
+++ Src/Server/Infrastructure/Interface/IServicePrivoder.cs
@@ -1,7 +1,12 @@
+using Autofac;
+
 namespace Server.Infrastructure.Interface
 {
     public interface IServicePrivoder
     {
-         
+        /// <summary>
+        /// IoC 容器
+        /// </summary>
+        IContainer Container { get; set; }
     }
 }
