29ed3b0b6191400f935472f570661b11f8341525
Alger <AlgerDu@outlook.com>
2017-06-18 10:15:08 +0800

    暂定，尚未使用

---
+++ Src/Server/Models/Domain/Base/BaseAggregateRoot.cs
@@ -0,0 +1,12 @@
+using System;
+using MyZone.Server.Infrastructure.Interface;
+
+namespace MyZone.Server.Models.Domain.Base
+{
+    /// <summary>
+    /// 领域根基类
+    /// </summary>
+    public class BaseAggregateRoot //: IAggregateRoot
+    {
+    }
+}
