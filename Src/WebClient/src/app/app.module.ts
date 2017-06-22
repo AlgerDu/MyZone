976adb7abb930724b1389976a9f6ff15a0cf769f
Alger <AlgerDu@outlook.com>
2017-06-22 23:03:21 +0800

    前端模板

--- Src/WebClient/src/app/app.module.ts
+++ Src/WebClient/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
-import { TabsModule } from 'ngx-bootstrap';
+import { TabsModule, AccordionModule } from 'ngx-bootstrap';
 
 @NgModule({
   declarations: [
@@ -11,7 +11,8 @@ import { TabsModule } from 'ngx-bootstrap';
   ],
   imports: [
     BrowserModule,
-    TabsModule.forRoot()
+    TabsModule.forRoot(),
+    AccordionModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
