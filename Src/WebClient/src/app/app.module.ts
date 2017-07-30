1934282fedcec4526cc018b5a27fca03f0cfa91d
Alger <AlgerDu@outlook.com>
2017-07-30 17:45:24 +0800

    http 通用处理服务

--- Src/WebClient/src/app/app.module.ts
+++ Src/WebClient/src/app/app.module.ts
@@ -1,5 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule, JsonpModule } from '@angular/http';
 
 import { TabsModule, AccordionModule, TooltipModule } from 'ngx-bootstrap';
 
@@ -20,6 +22,9 @@ import { NovelEditComponent } from './novel-edit/novel-edit.component';
   ],
   imports: [
     BrowserModule,
+    FormsModule,
+    HttpModule,
+    JsonpModule,
     AppRoutingModule,
     TabsModule.forRoot(),
     AccordionModule.forRoot(),
---
+++ Src/WebClient/src/app/service/http.service.ts
@@ -0,0 +1,62 @@
+import { Injectable, OnInit } from '@angular/core';
+import { Http, Response, RequestOptionsArgs, RequestOptions, Headers } from '@angular/http';
+
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+
+/**
+ * http 请求 帮助服务
+ * 主要用于对 HTTP 请求的通用处理
+ * @export
+ * @class HttpService
+ */
+@Injectable()
+export class HttpService implements OnInit {
+
+    constructor(
+        private http: Http
+    ) { }
+
+    ngOnInit(): void {
+    }
+
+    /** 封装 angular 的 http ，用来做一些默认的处理 */
+    post(url: string, body: any, options?: RequestOptionsArgs): Observable<Result> {
+
+        if (options == null) {
+            let headers = new Headers({ 'Content-Type': 'application/json' });
+            options = new RequestOptions({ headers: headers });
+        }
+
+        return this.http.post(url, body, options)
+            .map(this.extractData)
+            .catch(this.handleError);
+    }
+
+    /** 将 http 请求回来的数据包装成 json 对象 */
+    private extractData(res: Response) {
+        let body = res.json();
+        return body.data || {};
+    }
+
+    /** 处理 http 的请求错误 */
+    private handleError(error: Response | any) {
+        let errMsg: string;
+        if (error instanceof Response) {
+            const body = error.json() || '';
+            const err = body.error || JSON.stringify(body);
+            errMsg = `${error.status} - ${error.statusText || ''} ${err}`;
+        } else {
+            errMsg = error.message ? error.message : error.toString();
+        }
+        console.error(errMsg);
+        return Observable.throw(errMsg);
+    }
+}
+
+export class Result {
+    code: string;
+    message: string;
+    data: any;
+}
