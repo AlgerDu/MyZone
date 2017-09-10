0da6f0c8cccf4592a62cd0a08944e0ceb9cd196e
Alger <AlgerDu@outlook.com>
2017-09-10 23:13:30 +0800

    主要文件迁移工作

--- Src/Server/Startup.cs
+++ Src/Server/Startup.cs
@@ -7,69 +7,34 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
-using Microsoft.EntityFrameworkCore;
-using MyZone.Server.Models.DataBase;
-using MyZone.Server;
-using Autofac;
-using Autofac.Extensions.DependencyInjection;
-using AutoMapper;
+using Microsoft.Extensions.Options;
 
-namespace Server
+namespace MyZone.Server
 {
     public class Startup
     {
-        public Startup(IHostingEnvironment env)
+        public Startup(IConfiguration configuration)
         {
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables();
-            Configuration = builder.Build();
+            Configuration = configuration;
         }
 
-        public IConfigurationRoot Configuration { get; }
-
-        public IContainer ApplicationContainer { get; private set; }
+        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public IServiceProvider ConfigureServices(IServiceCollection services)
+        public void ConfigureServices(IServiceCollection services)
         {
-            //添加数据上下文
-            // services.AddDbContext<MyZoneContext>(options =>
-            //     options.UseNpgsql(Configuration.GetConnectionString("Default"))
-            // );
-
-            // Add framework services.
-            services
-                .AddMvc()
-                .AddJsonOptions(jsonOptions =>
-                {
-                    jsonOptions.SerializerSettings.NullValueHandling = Newtonsoft.Json.NullValueHandling.Ignore;
-                });
-
-            services.AddAutoMapper();
-
-            return services.MyZoneServices();
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            loggerFactory.AddFile("logs/myZone-{Date}.txt");
-
-            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
-            loggerFactory.AddDebug();
-
-            app.UseDefaultFiles();
-            app.UseStaticFiles();
-
-            app.UseMvc(routes =>
+            if (env.IsDevelopment())
             {
-                routes.MapRoute(
-                    name: "default",
-                    template: "{controller=Home}/{action=Index}/{id?}");
-            });
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
         }
     }
 }
