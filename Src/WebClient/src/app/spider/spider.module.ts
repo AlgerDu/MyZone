e111b5792730d1ab83a58bb941a424f3dbbd6869
Alger <AlgerDu@outlook.com>
2017-09-14 22:38:00 +0800

    可以运行

--- Src/WebClient/src/app/spider/spider.module.ts
+++ Src/WebClient/src/app/spider/spider.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { SpiderRoutingModule } from './spider-routing.module';
 import { NovelListComponent } from './novel/list/novel-list.component';
 import { NovelManagementService } from './novel/novel-management.srvice';
+import { BrowserModule } from '@angular/platform-browser';
 
 @NgModule({
     imports: [
+        BrowserModule,
         SpiderRoutingModule
     ],
     declarations: [
